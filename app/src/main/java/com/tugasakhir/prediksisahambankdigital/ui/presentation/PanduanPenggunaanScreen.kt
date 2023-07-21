8d36d6e91ac7475ab47629f8c937ed101a59b331
paulinagh <paulinanel50@gmail.com>
2023-07-21 17:43:27 +0700

    Revising according to the respondents' suggestion(s): - Adding close for LSTM and GRU explanation

--- app/src/main/java/com/tugasakhir/prediksisahambankdigital/ui/presentation/PanduanPenggunaanScreen.kt
+++ app/src/main/java/com/tugasakhir/prediksisahambankdigital/ui/presentation/PanduanPenggunaanScreen.kt
@@ -17,8 +17,9 @@ private val panduanPenggunaanList =
     mapOf(
         "https://tugas-akhir.portfoliobypgh.my.id/image/1.png" to "Halaman beranda memiliki tiga fitur:\n1. Memilih saham bank digital untuk diprediksi.\n2. Menampilkan hasil prediksi harga penutupan saham bank digital tersebut.",
         "https://tugas-akhir.portfoliobypgh.my.id/image/2a.png" to "Pada halaman perbandingan prediksi, fitur ini bertujuan untuk menampilkan perbandingan hasil prediksi harga penutupan bank digital yang dipilih sebelumnya.",
-        "https://tugas-akhir.portfoliobypgh.my.id/image/2b.png" to "Pada halaman perbandingan prediksi, fitur ini bertujuan untuk menampilkan grafik histori harga penutupan saham tersebut. Opsi di bawah bertujuan untuk menampilkan durasi grafik.",
-        "https://tugas-akhir.portfoliobypgh.my.id/image/2c.png" to "Pada halaman perbandingan prediksi, fitur ini bertujuan untuk menampilkan profil informasi saham bank digital tersebut yang terdiri dari gambar ikon, nama perusahaan, ringkasan, dan sebagainya.",
+        "https://tugas-akhir.portfoliobypgh.my.id/image/2b.png" to "Pada halaman perbandingan prediksi, fitur ini bertujuan untuk menampilkan grafik prediksi harga penutupan saham tersebut. Opsi di bawah bertujuan untuk menampilkan hasil prediksi berdasarkan algoritma LSTM atau GRU.",
+        "https://tugas-akhir.portfoliobypgh.my.id/image/2c.png" to "Pada halaman perbandingan prediksi, fitur ini bertujuan untuk menampilkan grafik histori harga penutupan saham tersebut. Opsi di bawah bertujuan untuk menampilkan durasi grafik.",
+        "https://tugas-akhir.portfoliobypgh.my.id/image/2d.png" to "Pada halaman perbandingan prediksi, fitur ini bertujuan untuk menampilkan profil informasi saham bank digital tersebut yang terdiri dari gambar ikon, nama perusahaan, ringkasan, dan sebagainya.",
         "https://tugas-akhir.portfoliobypgh.my.id/image/3.png" to "Halaman informasi akan menampilkan menu-menu seputar aplikasi dan pengembangnya."
     )
 
